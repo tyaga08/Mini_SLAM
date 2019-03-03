1dc194f200a5692323f3ad7b056baf9cc024b0eb
tyaga08 <rtyagaraja@gmail.com>
2019-03-03 03:42:13 -0500

    added pcl and synthesize_point_cloud function

--- include/include_files/include_files.h
+++ include/include_files/include_files.h
@@ -4,5 +4,7 @@
 #include<nav_msgs/OccupancyGrid.h>
 #include<sensor_msgs/PointCloud2.h>
 #include<sensor_msgs/PointField.h>
+#include<pcl/point_types.h>
+#include<pcl/point_cloud.h> 
 
 using namespace std;
--- include/source_code/point_cloud.cpp
+++ include/source_code/point_cloud.cpp
@@ -3,14 +3,20 @@
 
 class point_cloud {
 private:
-		sensor_msgs::PointCloud2 pcloud;// = new sensor_msgs::PointCloud2();
+		sensor_msgs::PointCloud2 pcloud;
 		shared_ptr<occupancy_grid> oc_grid;
+		pcl::PointCloud<pcl::PointXY> cloud;
 	
 
 public:
 
 	point_cloud(ros::NodeHandle &nh, ros::Publisher &pc_publisher) {
 		oc_grid = shared_ptr<occupancy_grid>(new occupancy_grid());
+
+		cloud.height = 3;
+		cloud.width = 5;
+		cloud.is_dense = true;
+		cloud.points.resize(cloud.height*cloud.width);
 
 		pcloud.height = 1;
 		pcloud.width = 1;
@@ -38,8 +44,16 @@ public:
 		pc_publisher = nh.advertise<sensor_msgs::PointCloud2>("/cloud", 1);
 	}
 
+	~point_cloud() {}
 
-	void publish_data(ros::Publisher &pc_publisher, int &count, int &check_count) {
+	void publish_data(ros::Publisher &pc_publisher, int &count, int &check_count);
+	
+	void synthesize_point_cloud(vector<pair<int8_t,int8_t> > &occupied_points);
+
+};
+
+
+void point_cloud::publish_data(ros::Publisher &pc_publisher, int &count, int &check_count) {
 		if(count!=check_count)
 			oc_grid->navigate_robot("forward");
 
@@ -49,7 +63,16 @@ public:
 			oc_grid->navigate_robot("left");
 		}
 
+		vector<pair<int8_t,int8_t> > occupied_points = oc_grid->lidar_scan();
+
+		synthesize_point_cloud(occupied_points);
+
 		pc_publisher.publish(pcloud);
 	}
 
-};
+void point_cloud::synthesize_point_cloud(vector<pair<int8_t,int8_t> > &occupied_points) {
+	for(uint i=0; i<occupied_points.size(); i++) {
+		cloud.points[i].x = occupied_points.at(i).first;
+		cloud.points[i].y = occupied_points.at(i).second;
+	}
+}
